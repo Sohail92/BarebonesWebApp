b8b41fae659c617eb8fa9c6e4b2b153f5991d5b7
Sohail Rahman <contact@sohailrahman.net>
2018-12-02 11:28:50 +0000

    further simplified file structure

--- TestWebApp/Global.asax.cs
+++ TestWebApp/Global.asax.cs
@@ -1,5 +1,4 @@
-﻿using System.Web.Mvc;
-using System.Web.Routing;
+﻿using System.Web.Routing;
 
 namespace TestWebApp
 {
@@ -7,8 +6,6 @@ namespace TestWebApp
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
