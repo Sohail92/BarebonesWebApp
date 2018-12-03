4b88115b94f16e7c0a8755202b7d0bd862fd9bab
Sohail Rahman <contact@sohailrahman.net>
2018-12-03 21:13:17 +0000

    added image grabbing code

--- TestWebApp/src/index.ts
+++ TestWebApp/src/index.ts
@@ -1,4 +1,21 @@
 ﻿import "./index.css";
-import "jquery";
+import * as $ from "jquery";
 import "popper.js"
 import "bootstrap";
+
+var images = <any><HTMLInputElement>document.getElementById("imgContainer");
+
+$.ajax({
+    type: "GET",
+    url: "http://localhost:55828/api/values",
+     dataType: 'json',
+    success: function (json) {
+        alert("Successfully got images");
+        images.myImages = json;
+        console.log(images.myImages);
+    },
+    error: function (xhr, ajaxOptions, thrownError) {
+        console.log(xhr.status);
+        console.log(thrownError);
+    }
+});
