faa82dbfaf07e41c990f399e50faf86b2d9deff0
Sohail Rahman <contact@sohailrahman.net>
2018-12-04 22:18:03 +0000

    added some code for ajax call and dynamic carousel

--- TestWebApp/src/index.ts
+++ TestWebApp/src/index.ts
@@ -3,19 +3,38 @@ import * as $ from "jquery";
 import "popper.js"
 import "bootstrap";
 
-var images = <any><HTMLInputElement>document.getElementById("imgContainer");
+$(document).ready(function () {
+    var result = $.ajax({
+        type: "GET",
+        url: "http://localhost:55828/api/values",
+        dataType: 'json',
+        success: function (json) {
+            alert("Successfully got images");
+            result = json;
+            console.log("ajax completed: result = " + result);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log(xhr.status);
+            console.log(thrownError);
+        }
+    });
+    
+    debugger;
 
-$.ajax({
-    type: "GET",
-    url: "http://localhost:55828/api/values",
-     dataType: 'json',
-    success: function (json) {
-        alert("Successfully got images");
-        images.myImages = json;
-        console.log(images.myImages);
-    },
-    error: function (xhr, ajaxOptions, thrownError) {
-        console.log(xhr.status);
-        console.log(thrownError);
+    console.log("Result = " + result);
+
+    for (let j = 0; j < 3; j++) {
+        $('<div class="carousel-item"><img src="https://via.placeholder.com/700x300.png?text=i0" width="100%"></div>').appendTo('.carousel-inner');
+        $('<li data-target="#carousel" data-slide-to="' + j + '"></li>').appendTo('.carousel-indicators')
     }
+    $('.carousel-item').first().addClass('active');
+    $('.carousel-indicators > li').first().addClass('active');
+    $('#carousel').carousel();
+
+    $('.carousel-control-prev-icon').click(function () { $('#carousel').carousel('prev'); });
+
+    $('.carousel-control-next-icon').click(function () { $('#carousel').carousel('next'); });
+
+    $('.carousel-control').click(function (e) { e.preventDefault(); });
+
 });
